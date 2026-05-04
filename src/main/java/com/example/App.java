a48fb2d0752b88bac8ac6a98270966b5d256e104
Nguyễn Văn Mạnh <25020257@vnu.edu.vn>
2026-05-04 23:02:10 +0700

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,8 +5,8 @@ import org.slf4j.LoggerFactory;
 public class App {
     private static final Logger logger = LoggerFactory.getLogger(App.class);
     public static void main(String[] args) {
-        logger.info("Dang chay ung dung Maven...");
-        System.out.println("Ket qua phep tinh: " + sum(5, 5));
+        logger.info("Ứng dụng khởi chạy thành công!");
+        logger.info("Kết quả phép cộng: {}", add(10, 5));
     }
-    public static int sum(int a, int b) { return a + b; }
+    public static int add(int a, int b) { return a + b; }
 }
