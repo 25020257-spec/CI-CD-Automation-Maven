9dbd8a3f9f588fd437ba65ed83e30ca3e9e4dc70
Nguyễn Văn Mạnh <25020257@vnu.edu.vn>
2026-05-04 22:15:22 +0700

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,1 +1,12 @@
+package com.example;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class App {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+    public static void main(String[] args) {
+        logger.info("Dang chay ung dung Maven...");
+        System.out.println("Ket qua phep tinh: " + sum(5, 5));
+    }
+    public static int sum(int a, int b) { return a + b; }
+}
